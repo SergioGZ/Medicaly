2f155924439ccb774f03e7e7b3b03d0c3fa35df7
Sergio González <sergiogg97@hotmail.com>
2024-09-03 19:51:37 +0200

    Server URL fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ENV = {
-  SERVER_HOST: "strapi-medicaly-production.up.railway.app",
-  API_URL: "strapi-medicaly-production.up.railway.app/api",
+  SERVER_HOST: "http://strapi-medicaly-production.up.railway.app",
+  API_URL: "https://strapi-medicaly-production.up.railway.app/api",
   ENDPOINTS: {
     AUTH: {
       REGISTER: "auth/local/register",
